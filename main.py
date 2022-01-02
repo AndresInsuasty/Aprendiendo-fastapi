a3c39598f6502a0a76719c6697d9776c8fef5dbb
Andres <andresinsuastyd10@gmail.com>
2022-01-02 16:20:49 -0500

    validations_path

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-#Python
+# Python
 from typing import Optional
 
 
-#Pydantic
+# Pydantic
 from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body
-from fastapi.param_functions import Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
 # Models
+
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -20,19 +21,45 @@ class Person(BaseModel):
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
 
+
 @app.get("/")
 def home():
-    return {"Hello":"World"}
+    return {"Hello": "World"}
 
 # Request and Response Body
+
+
 @app.post("/person/new")
-def create_person(person:Person=Body(...)):
+def create_person(person: Person = Body(...)):
     return person
 
 # Validaciones: Query Parameters
+
+
 @app.get("/person/detail")
 def show_person(
-    name:Optional[str] = Query(None,min_length=1,max_length=50),
-    age:str = Query(...)
+    name: Optional[str] = Query(None, 
+        min_length=1,
+        max_length=50,
+        title="Person name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: str = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's requerid"
+        )
 ):
-    return {name:age}
+    return {name: age}
+
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ..., 
+        gt=0,
+        title="Person Id",
+        description="This is the person Id, It's an integer"
+        )
+):
+    return {person_id: "It exist"}
