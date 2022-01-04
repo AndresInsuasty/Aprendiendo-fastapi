b82cccb3cfee586c11cbcd7543cb0182a62e0bf1
Andres <andresinsuastyd10@gmail.com>
2022-01-03 21:34:03 -0500

    status_code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 from fastapi import Body, Query, Path
 
 app = FastAPI()
@@ -55,21 +56,30 @@ class PersonOut(PersonBase):
     pass
 
 
-@app.get("/")
+@app.get(
+    path="/",
+    status_code=status.HTTP_200_OK
+    )
 def home():
     return {"Hello": "World"}
 
 # Request and Response Body
 
 
-@app.post("/person/new",response_model=PersonOut)
+@app.post(
+    path="/person/new",
+    response_model=PersonOut,
+    status_code=status.HTTP_201_CREATED
+    )
 def create_person(person: Person = Body(...)):
     return person
 
 # Validaciones: Query Parameters
 
 
-@app.get("/person/detail")
+@app.get(
+    path="/person/detail",
+    status_code=status.HTTP_200_OK)
 def show_person(
     name: Optional[str] = Query(None, 
         min_length=1,
@@ -89,7 +99,10 @@ def show_person(
     return {name: age}
 
 
-@app.get("/person/detail/{person_id}")
+@app.get(
+    path="/person/detail/{person_id}",
+    status_code=status.HTTP_200_OK
+    )
 def show_person(
     person_id: int = Path(
         ..., 
@@ -102,7 +115,10 @@ def show_person(
     return {person_id: "It exist"}
 
 # Validaciones: Request Body
-@app.put("/person/{person_id}")
+@app.put(
+    path="/person/{person_id}",
+    status_code=status.HTTP_202_ACCEPTED
+    )
 def update_person(
     person_id: int = Path(
         ...,
